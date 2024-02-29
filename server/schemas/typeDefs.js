8e01856d00e3c4207b84f5b3a572d49ea46331f9
estan <elise.stankus@gmail.com>
2024-02-28 22:53:49 -0800

    added query and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,6 +22,17 @@ const typeDefs = `
         token: ID!
         user: User
     }
+    
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: String!, authors: [String], description: String!, image: String, link: String, title: String!): User
+        removeBook(bookId: String!): User 
+    }
 
 `
 
