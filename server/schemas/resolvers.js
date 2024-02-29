4342d5121ddadb624f8a7b7fdb95bd3a7fa65bea
estan <elise.stankus@gmail.com>
2024-02-28 23:04:37 -0800

    added me query to find logged in user using context functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,15 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        // query to find logged in user using context from JWT data
+        me: async(parent, args, context) => {
+            if (context.user) {
+                return User.findOne({
+                    _id: context.user._id
+                });
+            }
+            throw AuthenticationError;
+        },
     },
 
     Mutation: {
