f6212eab03ddf359be644c217e4cce0132fd8663
estan <elise.stankus@gmail.com>
2024-02-28 23:11:37 -0800

    added login and addUser mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,7 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
     Query: {
         // query to find logged in user using context from JWT data
-        me: async(parent, args, context) => {
+        me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({
                     _id: context.user._id
@@ -15,6 +15,23 @@ const resolvers = {
     },
 
     Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw AuthenticationError
+            }
+            const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw AuthenticationError
+            }
+            const token = signToken(user);
+            return { token, user }
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
 
     },
 };
