68266cc20b078b9481384f6f908019b90ffb4cd4
estan <elise.stankus@gmail.com>
2024-02-28 22:33:54 -0800

    added User, Book, and Auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,27 @@
 const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        bookCount: Int
+        savedBooks: [Book]
+
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 
 `
 
