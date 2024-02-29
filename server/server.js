11bc8302f579d226101953f557e79f6bac934619
estan <elise.stankus@gmail.com>
2024-02-29 00:19:28 -0800

    moved middleware inside new apollo server instance, added graphQL url console log

--- server/server.js
+++ server/server.js
@@ -21,23 +21,29 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {console.log(`Api server listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
+  });
+  });
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
-
+// call function to start server
 startApolloServer();
